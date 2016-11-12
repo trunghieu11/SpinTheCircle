727882e525eacb9bd5dad0c3befdb080779d97ea
trunghieu11 <vuondenthanhcong11@gmail.com>
2016-11-12 13:25:25 +0700

    Publish game to Play Store

---
+++ Assets/SpinTheCircle/UI/Scripts/UIScripts/ButtonExit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AppAdvisory.UI {
+
+    /// <summary>
+	/// Attached to exit button
+	/// </summary>
+    public class ButtonExit : MonoBehaviour {
+        
+        public void OnClickedExitButton() {
+            Application.Quit();
+        }
+    }
+}
